0b85407a5c131735337fe59fa4c4b9bc07be7636
Jiwon Chang <me@jiwonc.net>
2022-07-18 16:01:22 -0400

    add favicon

--- .eleventy.js
+++ .eleventy.js
@@ -3,6 +3,7 @@ const markdownIt = require("markdown-it");
 module.exports = function(eleventyConfig) {
     /* Copy some files directly */
     eleventyConfig.addPassthroughCopy("src/style.css");
+    eleventyConfig.addPassthroughCopy("src/favicon.ico");
     eleventyConfig.addPassthroughCopy("src/assets");
     /* Make .md files more powerful */
     let mdoptions = {
