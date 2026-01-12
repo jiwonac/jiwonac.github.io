aca61aba697eacd76bb93256c4d5eaabfb0a87c3
Jiwon Chang <me@jiwonc.net>
2026-01-11 21:39:32 -0500

    Improved site SEO
    - Added sitemap XML generated using nunjucks
    - Added author, opengraph, and twitter meta tags in _head.html
    - Added schema.org structured data in _head.html
    - Changed canonical url to jiwonac.github.io
    - Added robots.txt pointing to sitemap

--- .eleventy.js
+++ .eleventy.js
@@ -38,6 +38,7 @@ module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy("src/_scripts/rough-cards.js");
     eleventyConfig.addPassthroughCopy("src/_scripts/microblog-cards.js");
     eleventyConfig.addPassthroughCopy("src/favicon.ico");
+    eleventyConfig.addPassthroughCopy("src/robots.txt");
     eleventyConfig.addPassthroughCopy("src/assets");
     eleventyConfig.addPassthroughCopy("src/admin");
 
